19ecb2a5d33072f2662a097f4ab63fc503ad8573
Sagor Sarker <sagorhem3532@gmail.com>
2019-01-09 12:11:01 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,70 @@
+import sys
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+from keras.models import load_model
+#from model.py import model
+
+# load ascii text and covert to lowercase
+filename = "data/wonderland.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+
+# create mapping of unique chars to integers
+chars = sorted(list(set(raw_text)))
+
+
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print ("Total Characters: ", n_chars)
+print ("Total Vocab: ", n_vocab)
+
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 100
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+print ("Total Patterns: ", n_patterns)
+
+# load the network weights
+filename = "weights-improvement-10-2.3512.hdf5"
+
+model = load_model(filename)
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+int_to_char = dict((i, c) for i, c in enumerate(chars))
+
+# pick a random seed
+start = numpy.random.randint(0, len(dataX)-1)
+pattern = dataX[start]
+print("Seed:")
+print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
+# generate characters
+for i in range(1000):
+	x = numpy.reshape(pattern, (1, len(pattern), 1))
+	x = x / float(n_vocab)
+	prediction = model.predict(x, verbose=0)
+	index = numpy.argmax(prediction)
+	result = int_to_char[index]
+	seq_in = [int_to_char[value] for value in pattern]
+	sys.stdout.write(result)
+	pattern.append(index)
+	pattern = pattern[1:len(pattern)]
+print ("\nDone.")
+
+
+
+
+
