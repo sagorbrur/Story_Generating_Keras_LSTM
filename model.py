2c2b9ba7049ff36562dee22dcbdd346c5d2d9171
Sagor Sarker <sagorhem3532@gmail.com>
2019-01-09 12:17:54 +0600

    Update model.py

--- model.py
+++ model.py
@@ -56,38 +56,5 @@ model.fit(X, y, epochs=10, batch_size=128, callbacks=callbacks_list)
 
 #For Testing run the test.py code
 
-#Testing
-
-'''
-
-# load the network weights
-filename = "weights-improvement-19-1.9435.hdf5"
-model.load_weights(filename)
-model.compile(loss='categorical_crossentropy', optimizer='adam')
-
-int_to_char = dict((i, c) for i, c in enumerate(chars))
-
-# pick a random seed
-start = numpy.random.randint(0, len(dataX)-1)
-pattern = dataX[start]
-print("Seed:")
-print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
-# generate characters
-for i in range(1000):
-	x = numpy.reshape(pattern, (1, len(pattern), 1))
-	x = x / float(n_vocab)
-	prediction = model.predict(x, verbose=0)
-	index = numpy.argmax(prediction)
-	result = int_to_char[index]
-	seq_in = [int_to_char[value] for value in pattern]
-	sys.stdout.write(result)
-	pattern.append(index)
-	pattern = pattern[1:len(pattern)]
-print ("\nDone.")
-
-'''
-
-
-
 
 
